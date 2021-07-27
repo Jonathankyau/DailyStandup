cb6b52a693b432cd9a0239cfb947b853ba1c8000
Jonathan Yau <jonathankyau@gmail.com>
2021-07-27 15:37:18 -0700

    Sort by Last Char

---
+++ 13.js
@@ -0,0 +1,22 @@
+// CHALLENGE
+// Sort by Last Char
+// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// Test.assertSimilar(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+// Test.assertSimilar(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+// Test.assertSimilar(last('take me to semynak'), ['take', 'me', 'semynak', 'to']); 
+
+// Hint: you have full control over a and b inside the sort method
+
+// Solution
+
+function last(x){
+    return x.split(" ").sort((a,b) => a[a.length -1] > b[b.length -1])   
+   
+ }
