849921ea032ed1342afd2fe6a77d18af17c0e101
Jonathan Yau <jonathankyau@gmail.com>
2022-11-29 19:30:37 -0800

    DS21

---
+++ DS21.js
@@ -0,0 +1,64 @@
+// Daily Standup Problem
+
+// Day 21
+// From: https://www.codewars.com/kata/56747fd5cb988479af000028
+
+// DESCRIPTION:
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+
+
+//Solution and PREP:
+function getMiddle(s)
+// {
+//   let newArr = s.split('') // [t, e, s, t]
+//   if (newArr.length % 2 === 0) {
+//     //even -> return middle two characters
+//     let middleChar = [newArr[newArr.length/2 -1], newArr[newArr.length/2]].join('')
+//     return middleChar
+//   } else {
+//     //odd -> return middle character
+//     let middleChar = newArr[Math.floor(newArr.length/2)]
+//     return middleChar
+//   }
+// }
+
+  {
+    if (s.length % 2 == 0) {
+      //even
+      let middleChar = s.charAt(s.length/2 -1) + s.charAt(s.length/2)
+      return middleChar
+    } else {
+      //odd
+      let middleChar = s.charAt(s.length/2)
+      return middleChar
+    }
+  }
+
+
+
+// Another solution
+
+  function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+
